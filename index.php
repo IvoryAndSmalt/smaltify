4cdaccbf082adeb46bd22ce3798f9ef6c2c72f0f
Lucas <lucas180395@gmail.com>
2019-02-11 18:16:56 +0100

    [+] some folders, gitignore and a notes files

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <!-- <style>
+        .wrapper{
+            display: grid;
+            grid-template-columns: 70% 30%;
+            grid-gap: 1em;
+        }
+
+        .wrapper > div{
+            background: grey;
+            padding: 10px;
+        }
+
+        .wrapper > div:nth-child(odd){
+            background: lightgrey;
+        }
+    
+    </style>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="main">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium accusamus dolor animi rerum libero harum iusto officiis doloribus, perferendis incidunt fuga architecto dignissimos? Deserunt culpa modi fuga voluptatibus ea dolore.
+    </div>
+    <div class="sidebar">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ipsa doloremque quis illum ut fuga praesentium quia laboriosam incidunt nulla, qui consequatur hic officia facilis minus voluptatem molestias aliquam eum.
+    </div>
+    <div class="main">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium accusamus dolor animi rerum libero harum iusto officiis doloribus, perferendis incidunt fuga architecto dignissimos? Deserunt culpa modi fuga voluptatibus ea dolore.
+    </div>
+    <div class="sidebar">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ipsa doloremque quis illum ut fuga praesentium quia laboriosam incidunt nulla, qui consequatur hic officia facilis minus voluptatem molestias aliquam eum.
+    </div>
+    <div class="main">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium accusamus dolor animi rerum libero harum iusto officiis doloribus, perferendis incidunt fuga architecto dignissimos? Deserunt culpa modi fuga voluptatibus ea dolore.
+    </div>
+    <div class="sidebar">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ipsa doloremque quis illum ut fuga praesentium quia laboriosam incidunt nulla, qui consequatur hic officia facilis minus voluptatem molestias aliquam eum.
+    </div>
+</div> -->
+
+<div class="header g-container m-5 p-5 justify black bg-beige mat-2-hv thin bdl-3-hv bd-red pointer hv-blue">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum laudantium dolor nisi! Et voluptatem laborum minus, nihil dolore accusamus ex perspiciatis debitis vel veritatis? A veritatis commodi soluta ipsa eum rem, velit quia tenetur quaerat qui labore vero vel nostrum, cumque harum nemo adipisci aut obcaecati eius. Sed, aperiam consequatur. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime possimus facilis porro? Iusto ex fuga repudiandae unde ut eligendi assumenda enim earum vel voluptas, accusamus accusantium similique ipsam eum. Quaerat?
+</div>
+
+<body>
+    
+    <div class="wrapper">
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique cum magni perferendis repellat, minima tempore omnis veritatis excepturi eveniet repellendus quibusdam est numquam aliquid alias ad quae officiis modi!</div>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique cum magni perferendis repellat, minima tempore omnis veritatis excepturi eveniet repellendus quibusdam est numquam aliquid alias ad quae officiis modi!</div>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique cum magni perferendis repellat, minima tempore omnis veritatis excepturi eveniet repellendus quibusdam est numquam aliquid alias ad quae officiis modi!</div>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique cum magni perferendis repellat, minima tempore omnis veritatis excepturi eveniet repellendus quibusdam est numquam aliquid alias ad quae officiis modi!</div>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique cum magni perferendis repellat, minima tempore omnis veritatis excepturi eveniet repellendus quibusdam est numquam aliquid alias ad quae officiis modi!</div>
+    </div>
+
+</body>
+</html>
